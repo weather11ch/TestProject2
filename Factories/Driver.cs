d5fa24400c75c2fddb0abf9369f3a6b84287824e
rchudaeva <weather11ch@gmail.com>
2024-06-13 19:54:42 +0300

    test

--- Factories/Driver.cs
+++ Factories/Driver.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Support;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using SeleniumExtras.WaitHelpers;
 
 namespace TestProject2.Factories
